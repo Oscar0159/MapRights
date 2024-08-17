2c66c368d8e85dabac9da02b32044feb1bbcef3d
Archer_Wn <mhqjeaock0649@gmail.com>
2024-08-18 05:00:56 +0800

    chore: use regex to get & check version

--- src/main/java/com/archerwn/mapRights/MapRights.java
+++ src/main/java/com/archerwn/mapRights/MapRights.java
@@ -13,6 +13,8 @@ import net.milkbowl.vault.economy.Economy;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class MapRights extends JavaPlugin {
 
@@ -29,11 +31,25 @@ public final class MapRights extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Get server version
+        Pattern versionPattern = Pattern.compile("(1)\\.(\\d+)(\\.\\d+)?");
+        Matcher matcher = versionPattern.matcher(getServer().getVersion());
+        int[] version = new int[2];
+        if (matcher.find()) {
+            version[0] = Integer.parseInt(matcher.group(1));
+            version[1] = Integer.parseInt(matcher.group(2));
+        } else {
+            getLogger().warning("Failed to get server version! Disabling MapRights...");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        // Setup managers
         ConfigManager.getInstance().setup(this);
         LangManager.getInstance().setup(this, ConfigManager.getInstance().getConfig().getString("language"));
         EconomyManager.getInstance().setup(this, ConfigManager.getInstance().getConfig().getBoolean("economy.enable"));
 
-        // Hook
+        // Hook [PlaceholderAPI]
         if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null &&
             Objects.requireNonNull(getServer().getPluginManager().getPlugin("PlaceholderAPI")).isEnabled()) {
             boolean success = new PAPIMapRightsHook(this).register();
@@ -44,10 +60,12 @@ public final class MapRights extends JavaPlugin {
             }
         }
 
+        // Register commands and listeners
         Objects.requireNonNull(getCommand("maprights")).setExecutor(new MapRightsCommand());
         Objects.requireNonNull(getCommand("maprights")).setTabCompleter(new MapRightsCommandCompleter());
 
-        if (getServer().getVersion().contains("1.21")) {
+        // Register listeners
+        if (version[0] == 1 && version[1] >= 21) {
             getServer().getPluginManager().registerEvents(new MapCopyListener_v1_21(), this);
         } else {
             getServer().getPluginManager().registerEvents(new MapCopyListener(), this);
